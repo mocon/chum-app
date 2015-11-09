0716680870e2fb7c3318097134f3155ec3336539
Myles O'Connor <myles@drocventures.com>
2015-11-08 16:04:36 -0800

    Chum-weighting UI working

--- app/scripts/controllers/chums.js
+++ app/scripts/controllers/chums.js
@@ -13,4 +13,14 @@ angular.module('chumApp')
     this.userChums = ContactsService.getChums();
     
     this.total = this.userChums.length;
+    
+    this.allRatings = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    
+    this.rateContact = function(index, newWeight){
+	    this.userChums.forEach(function(i){
+		    if (i._id === index) {
+			    i.weight = newWeight;
+		    }
+	    });
+    };
   });
--- app/scripts/controllers/contacts.js
+++ app/scripts/controllers/contacts.js
@@ -13,4 +13,14 @@ angular.module('chumApp')
     this.userContacts = ContactsService.getContacts();
     
     this.total = this.userContacts.length;
+    
+    this.allRatings = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    
+    this.rateContact = function(index, newWeight){
+	    this.userContacts.forEach(function(i){
+		    if (i._id === index) {
+			    i.weight = newWeight;
+		    }
+	    });
+    };
   });
